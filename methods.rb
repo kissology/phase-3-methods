00af90ba4946f405b9743459d976e046abc60528
Caitlin Ma <109817775+kissology@users.noreply.github.com>
2022-12-11 16:52:03 -0500

    commit tree 15b78ef830447ce1782892729ae21375a2c9bb21

--- methods.rb
+++ methods.rb
@@ -1,1 +1,34 @@
-# Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+
+end
+
+greet_programmer
+
+def greet(name)
+    puts "Hello #{name}!"
+end
+
+greet
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+greet_with_default
+greet_with_default "Naureen"
+
+
+def add (num1, num2)
+    puts num1 + num2
+    return num1 + num2
+end
+
+
+def halve(num)
+    return nil unless num.class == Integer
+    num / 2
+end
+
+halve(10)
+
